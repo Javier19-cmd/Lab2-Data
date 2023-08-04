573f583d1113c0a7c5cc2ad1b9706938473b2103
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-08-04 16:52:53 -0600

    primer push

---
+++ converter.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def convert_xlsx_to_csv(input_file, output_file):
+    # Leer el archivo XLSX utilizando pandas
+    df = pd.read_excel(input_file)
+    
+    # Guardar el DataFrame en un archivo CSV
+    df.to_csv(output_file, index=False)
+
+if __name__ == "__main__":
+    # Ruta del archivo XLSX de entrada
+    input_file_path = "./data.xlsx"
+    
+    # Ruta del archivo CSV de salida
+    output_file_path = "./data.csv"
+    
+    # Llamar a la función para convertir el archivo
+    convert_xlsx_to_csv(input_file_path, output_file_path)
