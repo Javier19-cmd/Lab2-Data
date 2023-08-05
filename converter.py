136177eb56e68b4864a0f5fdcd662c002d3c1622
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-08-05 11:49:30 -0600

    ejercicio 2

--- converter.py
+++ converter.py
@@ -9,10 +9,10 @@ def convert_xlsx_to_csv(input_file, output_file):
 
 if __name__ == "__main__":
     # Ruta del archivo XLSX de entrada
-    input_file_path = "./data.xlsx"
+    input_file_path = "./consumo.xlsx"
     
     # Ruta del archivo CSV de salida
-    output_file_path = "./data.csv"
+    output_file_path = "./consumo.csv"
     
     # Llamar a la función para convertir el archivo
     convert_xlsx_to_csv(input_file_path, output_file_path)
